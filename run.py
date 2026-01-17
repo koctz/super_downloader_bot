44efb6f53cb0560c9bab7a78b9e62cbbc7161678
koctz <137113690+koctz@users.noreply.github.com>
2026-01-17 19:20:23 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
+import asyncio
+import logging
+from src.bot import start_bot
 
+# Настройка логирования (чтобы видеть ошибки в консоли)
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(start_bot())
+    except KeyboardInterrupt:
+        print("🛑 Бот остановлен!")
