74442229c9f803a3aff544355a48bb24242456fa
koctz <137113690+koctz@users.noreply.github.com>
2026-01-18 13:28:52 +0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,25 +1,15 @@
 from aiogram import Bot, Dispatcher
 from src.config import conf
-from src.db import init_db
 from src.handlers.common import common_router
 from src.handlers.video import video_router
 
 async def start_bot():
-    init_db()
-    print("Используемый токен:", conf.bot_token)
-    # Инициализация бота
     bot = Bot(token=conf.bot_token, parse_mode="HTML")
-    
-    # Инициализация диспетчера
     dp = Dispatcher()
-    
-    # Регистрируем роутеры
-    # Порядок важен: специфичные обработчики лучше ставить раньше, общие - позже
-    dp.include_router(common_router)
-    dp.include_router(video_router)
 
-    
-    # Удаляем вебхуки (если были) и запускаем поллинг
+    dp.include_router(video_router)
+    dp.include_router(common_router)
+
     await bot.delete_webhook(drop_pending_updates=True)
     print("🤖 Бот запущен и готов к работе!")
     await dp.start_polling(bot)
