7d54b2f3f192e6043f5c90877612ad0009dbd391
koctz <137113690+koctz@users.noreply.github.com>
2026-01-18 13:38:20 +0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -4,12 +4,18 @@ from src.handlers.common import common_router
 from src.handlers.video import video_router
 
 async def start_bot():
-    bot = Bot(token=conf.bot_token, parse_mode="HTML")
+    # Инициализация бота
+    bot = Bot(token=conf.bot_token)
+    
+    # Инициализация диспетчера
     dp = Dispatcher()
-
+    
+    # Регистрируем роутеры
+    # Порядок важен: специфичные обработчики лучше ставить раньше, общие - позже
     dp.include_router(common_router)
     dp.include_router(video_router)
-
+    
+    # Удаляем вебхуки (если были) и запускаем поллинг
     await bot.delete_webhook(drop_pending_updates=True)
     print("🤖 Бот запущен и готов к работе!")
     await dp.start_polling(bot)
